5534371df26fd995ef671c48ab91164805512d76
huongnhd <huongnhd@lifull-tech.vn>
2020-02-13 17:04:48 +0900

    v004

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 setup(
     name='rocket-vocab',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(
         '.',
         exclude=[
@@ -14,13 +14,13 @@ setup(
         ]
     ),
     data_files=[("data",
-             ["data/3000words.json", "data/process_3000words.json"])],
+            ["data/3000words.json", "data/process_3000words.json"])],
     license='MIT',
     description='learing vocabulary on terminal',
     author='huongnhd',
     author_email = 'huong.nhdh@gmail.com',
     url = 'https://github.com/huongnhdh/rocket-vocab',
-    download_url = 'https://github.com/huongnhdh/rocket-vocab/archive/v001.tar.gz',
+    download_url = 'https://github.com/huongnhdh/rocket-vocab/archive/v004.tar.gz',
     keywords = ['vocab', 'terminal', 'true color'],
     install_requires=[
     ],
