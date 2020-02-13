2284801dcbf3c02a9333854ee258c9fcae2491fb
huongnhd <huongnhd@lifull-tech.vn>
2020-02-13 19:08:17 +0900

    v005

--- setup.py
+++ setup.py
@@ -2,25 +2,23 @@
 #!-*-coding:utf8-*-
 """File setup.py"""
 from setuptools import setup, find_packages
+from distutils.core import setup
 import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 setup(
     name='rocket-vocab',
-    version='0.0.4.1',
-    packages=find_packages(
-        '.',
-        exclude=[
-            '*.tests', '*.tests.*', 'tests.*', 'tests'
-        ]
-    ),
-    data_files=[("data",
-            ["data/3000words.json", "data/process_3000words.json"])],
+    version='0.0.5',
+    packages=['rocket_vocab'],
+    package_dir={'rocket_vocab': 'src/rocket_vocab'},
+    # package_data={'rocket_vocab': ['src/data/*.json']},
+    # package_data={'': ['*.txt', '*.json', '*.special', '*.huh']},
+    include_package_data=True,
     license='MIT',
     description='learing vocabulary on terminal',
     author='huongnhd',
     author_email = 'huong.nhdh@gmail.com',
     url = 'https://github.com/huongnhdh/rocket-vocab',
-    download_url = 'https://github.com/huongnhdh/rocket-vocab/archive/v004.1.tar.gz',
+    download_url = 'https://github.com/huongnhdh/rocket-vocab/archive/v005.tar.gz',
     keywords = ['vocab', 'terminal', 'true color'],
     install_requires=[
     ],
@@ -36,6 +34,6 @@ setup(
   ],
     entry_points="""
     [console_scripts]
-    rocket=app.rocket:main
+    rocket=rocket_vocab.rocket:main
     """
 )
--- src/rocket_vocab/rocket.py
+++ src/rocket_vocab/rocket.py
@@ -3,27 +3,38 @@ import json
 import random
 import os
 import sys
-from app.utils import get_dir_root
-# class Aword:
-#     # Initializer / Instance Attributes
-#     def __init__(self, word, know_point=0, example="", noted="", ipa=""):
-#         self.word = word
-#         self.know_point = know_point
-#         self.example = example
-#         self.noted = noted
-#         self.ipa = iba
+from rocket_vocab.utils import get_data_path
 
 
-class style():
+class Style():
+    @staticmethod
     def BLACK(x): return '\033[30m' + str(x)
+
+    @staticmethod
     def RED(x): return '\033[31m' + str(x)
+
+    @staticmethod
     def GREEN(x): return '\033[32m' + str(x)
+
+    @staticmethod
     def YELLOW(x): return '\033[33m' + str(x)
+
+    @staticmethod
     def BLUE(x): return '\033[34m' + str(x)
+
+    @staticmethod
     def MAGENTA(x): return '\033[35m' + str(x)
+
+    @staticmethod
     def CYAN(x): return '\033[36m' + str(x)
+
+    @staticmethod
     def WHITE(x): return '\033[37m' + str(x)
+
+    @staticmethod
     def UNDERLINE(x): return '\033[4m' + str(x)
+
+    @staticmethod
     def RESET(x): return '\033[0m' + str(x)
 
 
@@ -34,7 +45,7 @@ def _load_json_file(path_file_json):
 
 
 def load_training_data():
-    return _load_json_file(os.path.join(get_dir_root(), 'data', '3000words.json'))
+    return _load_json_file(os.path.join(get_data_path(), '3000words.json'))
 
 
 def load_process_data(process_path_data):
@@ -43,7 +54,7 @@ def load_process_data(process_path_data):
 
 def main():
     process_path_data = os.path.join(
-        get_dir_root(), 'data', 'process_3000words.json')
+        get_data_path(), 'process_3000words.json')
     wordsdata = load_training_data()
     processdata = load_process_data(process_path_data)
     if not processdata:
@@ -57,39 +68,39 @@ def main():
         # DO THINGS
         while number_word < 1:
             number_word = int(
-                input(style.GREEN("Many the words you want to train? > ")))
+                input(Style.GREEN("Many the words you want to train? > ")))
         for i in range(number_word):
             word_n = random.randint(0, length_wordlist-1)
             print("------------------------")
-            print(style.WHITE('Are you remember it? ') +
-                  style.GREEN(processdata[word_n]['word']))
+            print(Style.WHITE('Are you remember it? ') +
+                  Style.GREEN(processdata[word_n]['word']))
             print("------------------------")
             while True:
                 choice = input("y|n|uhm|c|  > ")
                 if choice == 'y':
-                    print(style.GREEN("\nGreat!"))
+                    print(Style.GREEN("\nGreat!"))
                     print(processdata[word_n]['example'])
                     processdata[word_n]['know_point'] = processdata[word_n]['know_point'] + 1
                     print('\n')
                     input()
                 if choice == 'n':
-                    print(style.YELLOW('\nOh no!'))
-                    print(style.YELLOW(
+                    print(Style.YELLOW('\nOh no!'))
+                    print(Style.YELLOW(
                         'https://www.google.com/search?q={}+l%C3%A0+g%C3%AC'.format(processdata[word_n]['word'])))
-                    print(style.YELLOW(
+                    print(Style.YELLOW(
                         'https://www.google.com/search?q=what+is+%22{}%22'.format(processdata[word_n]['word'])))
-                    print(style.YELLOW(
+                    print(Style.YELLOW(
                         'https://www.google.com/search?q=%22{}%22&tbm=isch'.format(processdata[word_n]['word'])))
                     print('\n')
                     input()
                 if choice == 'uhm':
                     if processdata[word_n]['ipa']:
-                      print(style.YELLOW('\n'+processdata[word_n]['ipa']))
+                      print(Style.YELLOW('\n'+processdata[word_n]['ipa']))
                     if processdata[word_n]['noted']:
-                      print(style.YELLOW(processdata[word_n]['noted']))
+                      print(Style.YELLOW(processdata[word_n]['noted']))
                     if processdata[word_n]['example']:
-                      print(style.YELLOW(processdata[word_n]['example']))
-                    print(style.YELLOW(
+                      print(Style.YELLOW(processdata[word_n]['example']))
+                    print(Style.YELLOW(
                         'https://www.google.com/search?q=what+is+%22{}%22'.format(processdata[word_n]['word'])))
                     print('\n')
                     input()
